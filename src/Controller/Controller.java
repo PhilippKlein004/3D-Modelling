49c071894da905b431cdcdb2812e036b2bab4fab
Philipp Klein <137300868+PhilippKlein004@users.noreply.github.com>
2024-08-30 23:57:17 +0200

    Added warning to Controller.java

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -8,6 +8,8 @@ import View.*;
  * @since 01.25.2024
  */
 
+// NOTE: JFrames may have compatibility issues on Windows (ARM),
+// potentially leading to malfunction in some cases.
 public abstract class Controller {
 
     public static void main(String[] args) {
