d81757abe56c28d7929e73507c5dbcb8530873c9
pklein2s <philipp.klein@smail.inf.h-brs.de>
2025-03-20 23:43:52 +0100

    The readability of the code in the class 'Cube.java' has been improved.

--- src/Model/Cube.java
+++ src/Model/Cube.java
@@ -17,21 +17,32 @@ public abstract class Cube {
 
         Structure cube = new Structure( true );
 
-        Node node1 = new Node(-100, -100, -100);
-        Node node2 = new Node(-100, -100, 100);
-        Node node3 = new Node(-100, 100, -100);
-        Node node4 = new Node(-100, 100, 100);
-        Node node5 = new Node(100, -100, -100);
-        Node node6 = new Node(100, -100, 100);
-        Node node7 = new Node(100, 100, -100);
-        Node node8 = new Node(100, 100, 100);
+        Node bottomLeftBack = new Node(-100, -100, -100);
+        Node bottomLeftFront = new Node(-100, -100, 100);
 
-        Node[] nodes = {node1, node2, node3, node4, node5, node6, node7, node8};
+        Node topLeftBack = new Node(-100, 100, -100);
+        Node topLeftFront = new Node(-100, 100, 100);
 
-        Node[][] relations = {  {node1, node2}, {node3, node4}, {node1, node3},
-                                {node2, node4}, {node6, node8}, {node5, node7},
-                                {node5, node6}, {node7, node8}, {node1, node5},
-                                {node3, node7}, {node4, node8}, {node2, node6}};
+        Node bottomRightBack = new Node(100, -100, -100);
+        Node bottomRightFront = new Node(100, -100, 100);
+
+        Node topRightBack = new Node(100, 100, -100);
+        Node topRightFront = new Node(100, 100, 100);
+
+        Node[] nodes = {bottomLeftBack, bottomLeftFront, topLeftBack, topLeftFront, bottomRightBack, bottomRightFront, topRightBack, topRightFront};
+
+        Node[][] relations = {  {bottomLeftBack, bottomLeftFront},
+                                {topLeftBack, topLeftFront},
+                                {bottomLeftBack, topLeftBack},
+                                {bottomLeftFront, topLeftFront},
+                                {bottomRightFront, topRightFront},
+                                {bottomRightBack, topRightBack},
+                                {bottomRightBack, bottomRightFront},
+                                {topRightBack, topRightFront},
+                                {bottomLeftBack, bottomRightBack},
+                                {topLeftBack, topRightBack},
+                                {topLeftFront, topRightFront},
+                                {bottomLeftFront, bottomRightFront} };
 
         cube.addNodes(nodes);
         cube.addRelations(relations);
